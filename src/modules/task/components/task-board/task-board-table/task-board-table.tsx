6fa30ddccb36c332565af0ca80333a0f35ab98d1
Leonardo Cristofani <leo.cristofani@gmail.com>
2021-09-21 16:52:27 -0300

    Connect tasks table to tasks state

--- src/modules/task/components/task-board/task-board-table/task-board-table.tsx
+++ src/modules/task/components/task-board/task-board-table/task-board-table.tsx
@@ -1,8 +1,9 @@
 import { Container, Grid } from "@material-ui/core";
 
-import { generateId } from "../../../../../shared/shared-utils";
-import { Task, TaskProgress } from "../../../task-types";
+import { TaskProgress, TaskPriority } from "../../../task-types";
 import TaskBoardColumn from "../task-board-column/task-board-column";
+import { useURL } from "../../../../../shared/url-privoder/url-provider";
+import useTasksState from "../../../providers/tasks-state/use-tasks-state";
 
 const taskProgressList: { label: string; progress: TaskProgress }[] = [
   { progress: "todo", label: "Todo" },
@@ -11,33 +12,21 @@ const taskProgressList: { label: string; progress: TaskProgress }[] = [
   { progress: "done", label: "Done" },
 ];
 
-const tasks: Task[] = [
-  {
-    id: generateId(),
-    title: "Task Title",
-    priority: "high",
-    progress: "done",
-    description: "This is the description and it's very long just to play with",
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-  },
-  {
-    id: generateId(),
-    title: "Task Title",
-    priority: "high",
-    progress: "done",
-    description: "This is the description and it's very long just to play with",
-    createdAt: Date.now(),
-    updatedAt: Date.now(),
-  },
-];
-
 export default function TaskBoardTable() {
+  const tasksState = useTasksState();
+  const url = useURL();
+
+  const priority = url.getParam("priority") as TaskPriority;
+
   return (
     <Container maxWidth="lg">
       <Grid container spacing={2}>
         {taskProgressList.map(({ label, progress }) => (
-          <TaskBoardColumn key={progress} label={label} tasks={tasks} />
+          <TaskBoardColumn
+            key={progress}
+            label={label}
+            tasks={tasksState.getMany({ progress, priority })}
+          />
         ))}
       </Grid>
     </Container>
